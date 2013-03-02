20e3ba477401cbc9f41fa1778b1b87f3f856d968
Victor <v.vovchenko91@gmail.com>
2013-03-02 17:04:35 +0200

    Added several methods for interacting with menu

---
+++ src/rarus/eatery/activity/MainActivity.java
@@ -0,0 +1,11 @@
+package rarus.eatery.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	}
+}
--- src/rarus/eatery/database/DBManager.java
+++ src/rarus/eatery/database/DBManager.java
@@ -1,7 +1,9 @@
 package rarus.eatery.database;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import rarus.eatery.model.DayMenu;
 import rarus.eatery.model.Dish;
 import rarus.eatery.model.Menu;
 import rarus.eatery.model.Order;
@@ -9,6 +11,7 @@ import rarus.eatery.model.OrderHeader;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
@@ -122,7 +125,7 @@ public class DBManager extends SQLiteOpenHelper {
 		query.append(DISHES_DESCRIPTION).append(" TEXT NOT NULL, ");
 		query.append(DISHES_PORTIONED).append(" INTEGER DEFAULT 0, ");
 		query.append(DISHES_PRICE).append(" FLOAT NOT NULL, ");
-		query.append(DISHES_RATING).append(" TEXT DEFAULT 0, ");
+		query.append(DISHES_RATING).append(" TEXT, ");
 		query.append(DISHES_PREORDER).append(" INTEGER DEFAULT 0);");		
 		_db.execSQL(query.toString());
 		
@@ -252,7 +255,7 @@ public class DBManager extends SQLiteOpenHelper {
 	/**
 	 * Удаляет все блюда
 	 */
-	public void deleteAllDish() {
+	public void deleteDishAll() {
 		mDb.beginTransaction();
 		try {
 			mDb.delete(TABLE_DISHES, null, null);
@@ -276,7 +279,6 @@ public class DBManager extends SQLiteOpenHelper {
 	public void addMenu(List<Menu> menu) {
 		StringBuilder query = new StringBuilder();
 		SQLiteStatement insertMenuStmt;
-		int date;
 		
 		query.append("INSERT INTO ").append(TABLE_MENU).append(" VALUES (?, ?, ?, ?, ?)");
 		insertMenuStmt = mDb.compileStatement(query.toString());
@@ -284,9 +286,8 @@ public class DBManager extends SQLiteOpenHelper {
 		mDb.beginTransaction();
 		try {
 			for (int i = 0; i < menu.size(); i++) {
-				date = menu.get(i).getDate() - (menu.get(i).getDate() % 86400);
 				insertMenuStmt.bindNull(1);
-				insertMenuStmt.bindString(2, Integer.toString(date));
+				insertMenuStmt.bindString(2, Integer.toString(formatDate(menu.get(i).getDate())));
 				insertMenuStmt.bindString(3, Integer.toString(menu.get(i).getDishId()));
 				insertMenuStmt.bindString(4, Float.toString(menu.get(i).getAvailable()));
 				insertMenuStmt.bindString(5, Integer.toString(menu.get(i).getTimestamp()));
@@ -300,7 +301,192 @@ public class DBManager extends SQLiteOpenHelper {
 			Log.i(LOG_TAG, "Added menu (" + Integer.toString(menu.size()) + ")");
 		}
 	}
+	
+	/**
+	 * Вовзращает количество дат на которые доступно меню
+	 * 
+	 * @return
+	 *     int с кол-вом дат
+	 */
+	public int getMenuDatesCount() {
+		StringBuilder query = new StringBuilder();
+		int result = 0;
+		
+		query.append("SELECT COUNT(DISTINCT ").append(MENU_DATE).append(") ");
+		query.append("FROM ").append(TABLE_MENU);
+		
+		mDb.beginTransaction();
+		try {
+			Cursor c = mDb.rawQuery(query.toString(), new String[] {});
+			mDb.setTransactionSuccessful();			
 
+			c.moveToFirst();
+			result = c.getInt(0);
+			c.close();
+		} finally {
+			mDb.endTransaction();
+		}
+		return result;
+	}
+	
+	/**
+	 * Возвращает список дат на которые доступно меню
+	 * 
+	 * @return
+	 *     {@link List} из {@link Integer} Unix time дат.
+	 *     Даты в Unix time формате. Отсортированы по возрастанию.
+	 */
+	public List<Integer> getMenuDates() {
+		List<Integer> result = new ArrayList<Integer>();
+		
+		mDb.beginTransaction();
+		try {
+			Cursor c = mDb.query(true, TABLE_MENU, new String[] {MENU_DATE}, null, null, 
+					null, null, MENU_DATE + " ASC", null);
+			mDb.setTransactionSuccessful();
+			
+			if (c.getCount() > 0) {
+				c.moveToFirst();
+				do {
+					result.add(c.getInt(0));
+				} while(c.moveToNext());
+				c.close();
+			}
+		} finally {
+			mDb.endTransaction();
+		}
+		return result;
+	}
+	
+	/**
+	 * Возвращает количество блюд в меню на определенную дату
+	 * 
+	 * @param date
+	 *     дата в Unix time формате
+	 * @return
+	 *     int кол-во блюд
+	 */
+	public int getMenuAtDateCount(int date) {
+		StringBuilder query = new StringBuilder();
+		int result = 0;
+		
+		query.append("SELECT COUNT(").append(MENU_DISH_ID).append(") FROM ");
+		query.append(TABLE_MENU).append(" WHERE ").append(MENU_DATE).append(" = ?");
+		
+		mDb.beginTransaction();
+		try {
+			Cursor c = mDb.rawQuery(query.toString(),
+					new String[] {Integer.toString(formatDate(date))});
+			mDb.setTransactionSuccessful();			
+
+			c.moveToFirst();
+			result = c.getInt(0);
+			c.close();
+		} finally {
+			mDb.endTransaction();
+		}
+		return result;
+	}
+	
+	/**
+	 * Возвращает меню на определенную дату
+	 * 
+	 * @param date
+	 *     дата в Unix time формате
+	 * @return
+	 *     {@link List} из объектов {@link DayMenu}
+	 */
+	public List<DayMenu> getMenuAtDate(int date) {
+		StringBuilder query = new StringBuilder();
+		List<DayMenu> result = new ArrayList<DayMenu>();
+		
+		query.append(TABLE_MENU).append(" AS MU INNER JOIN ").append(TABLE_DISHES);
+		query.append(" AS DS ON MU.").append(MENU_DISH_ID).append(" = DS.").append(KEY_ID);
+		
+		mDb.beginTransaction();
+		try {
+			Cursor c = mDb.query(false, query.toString(),
+					new String[] {"MU." + KEY_ID, "DS." + KEY_ID, DISHES_NAME,
+						DISHES_DESCRIPTION, DISHES_PORTIONED, DISHES_PRICE, DISHES_RATING,
+						DISHES_PREORDER, MENU_AVAILABLE, MENU_TIMESTAMP},
+						MENU_DATE + " = ?", new String[] {Integer.toString(formatDate(date))},
+						null, null, null, null);
+			mDb.setTransactionSuccessful();
+			
+			if (c.moveToFirst()) {
+				do {
+					result.add(new DayMenu(c.getInt(0), formatDate(date), c.getInt(1),
+							c.getString(2), c.getString(3), (c.getInt(4) == 0 ? false : true),
+							c.getFloat(5), c.getString(6), (c.getInt(7) == 0 ? false : true),
+							c.getFloat(8), c.getInt(9)));
+				} while(c.moveToNext());
+				c.close();
+			}
+		} finally {
+			mDb.endTransaction();
+		}
+		return result;
+	}
+	
+	/**
+	 * Возвращает временной штамп меню на определенную дату
+	 * 
+	 * @param date
+	 *     дата в Unix time формате
+	 * @return
+	 *     временной штамп
+	 */
+	public int getMenuTimestamp(int date) {
+		int result = 0;
+		
+		mDb.beginTransaction();
+		try {
+			Cursor c = mDb.query(TABLE_MENU, new String[] {MENU_TIMESTAMP}, MENU_DATE + " = ?",
+					new String[] {Integer.toString(formatDate(date))}, null, null, null, "1");
+			mDb.setTransactionSuccessful();
+			
+			if (c.moveToFirst()) result = c.getInt(0);
+			c.close();
+		} finally {
+			mDb.endTransaction();
+		}
+		return result;
+	}
+	
+	/**
+	 * Удаляет меню на определенную дату
+	 * 
+	 * @param date
+	 *     дата в Unix time формате
+	 */
+	public void deleteMenuAtDate(int date) {
+		mDb.beginTransaction();
+		try {
+			mDb.delete(TABLE_MENU, MENU_DATE + " = ?",
+					new String[] {Integer.toString(formatDate(date))});
+			mDb.setTransactionSuccessful();
+		} finally {
+			mDb.endTransaction();
+			
+			Log.i(LOG_TAG, "Deleted menu (date = " + Integer.toString(formatDate(date)) + ")");
+		}
+	}
+	
+	/**
+	 * Удаляет все меню
+	 */
+	public void deleteMenuAll() {
+		mDb.beginTransaction();
+		try {
+			mDb.delete(TABLE_MENU, null, null);
+			mDb.setTransactionSuccessful();
+		} finally {
+			mDb.endTransaction();
+			
+			Log.i(LOG_TAG, "Deleted all menu");
+		}
+	}
+	
 	
 	// Заголовки заказа
 	
@@ -379,5 +565,18 @@ public class DBManager extends SQLiteOpenHelper {
 			
 			Log.i(LOG_TAG, "Added orders (" + Integer.toString(orders.size()) + ")");
 		}
+	}
+	
+	
+	/**
+	 * Округляет время указанное в Unix time до 00:00
+	 * 
+	 * @param date
+	 *    дата в Unix time формате
+	 * @return
+	 *    округлённая дата в Unix time формате
+	 */
+	private int formatDate(int date) {
+		return date = date - (date % 86400);
 	}
 }
