c8cd7c4d06a79313f5a0251460d2f3e56c939a16
Dmitriy <BinnaryWolf@gmail.com>
2013-03-05 04:06:15 +0200

    Interface for doInBackground

--- src/rarus/eatery/service/ServiceAPI.java
+++ src/rarus/eatery/service/ServiceAPI.java
@@ -3,6 +3,7 @@ package rarus.eatery.service;
 import rarus.eatery.model.EateryConstants;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.util.Log;
 
 /**
  * Класс асинхронного выполнения метов сервиса
@@ -22,8 +23,22 @@ public class ServiceAPI extends AsyncTask<APIMessage, Object, APIMessage> {
 
 	@Override
 	protected APIMessage doInBackground(APIMessage... params) {
+		Log.d(EateryConstants.SERVICE_LOG_TAG, "[API] - Do in background start");
+		APIMessage result = null;
+		switch (params[0].getCode()) {
+		case EateryConstants.GET_MENU_CODE: {
+			Log.d(EateryConstants.SERVICE_LOG_TAG, "[API] - getMenuCode");
+			result=new APIMessage(EateryConstants.GET_MENU_CODE, getMenu());
+		}
+			break;
+		case EateryConstants.SET_ORDER_CODE: {
+			Log.d(EateryConstants.SERVICE_LOG_TAG, "[API] - setOrderCode");
+			result=new APIMessage(EateryConstants.SET_ORDER_CODE, setOrder());
+		}
+			break;
 
-		return null;
+		}
+		return result;
 	}
 
 }
