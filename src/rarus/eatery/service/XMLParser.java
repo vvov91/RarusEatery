d91583201e10200a83a135b34136d10ce8bb2ff5
Dmitriy <BinnaryWolf@gmail.com>
2013-03-15 04:19:53 +0200

    Ping parser

--- src/rarus/eatery/service/XMLParser.java
+++ src/rarus/eatery/service/XMLParser.java
@@ -26,52 +26,123 @@ import android.view.MenuItem;
  */
 
 public class XMLParser {
-	
-	public static String processHtmlError(String html){
-		try{
-		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-		factory.setNamespaceAware(true);
-		XmlPullParser xpp = factory.newPullParser();
-		xpp.setInput(new StringReader(html));			
-		xpp.next();
-		String name = "";
-		String error="";
-		while (xpp.getEventType() != XmlPullParser.END_DOCUMENT){
-			switch (xpp.getEventType()) {
-			case XmlPullParser.START_DOCUMENT:
-				break;
-			case XmlPullParser.START_TAG:
-				name = xpp.getName();
-				break;
-			case XmlPullParser.END_TAG:				
-				break;
 
-			case XmlPullParser.TEXT:
-				if (name.equals("h1")) {
-					error=xpp.getText();
-				}				
-				break;
-
-			default:
-				break;
-			}
+	public static String processHtmlError(String html) {
+		try {
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			factory.setNamespaceAware(true);
+			XmlPullParser xpp = factory.newPullParser();
+			xpp.setInput(new StringReader(html));
 			xpp.next();
-		}
+			String name = "";
+			String error = "";
+			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+				switch (xpp.getEventType()) {
+				case XmlPullParser.START_DOCUMENT:
+					break;
+				case XmlPullParser.START_TAG:
+					name = xpp.getName();
+					break;
+				case XmlPullParser.END_TAG:
+					break;
 
-		return error;
-	} catch (XmlPullParserException e) {
-		return null;
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		return null;
+				case XmlPullParser.TEXT:
+					if (name.equals("h1")) {
+						error = xpp.getText();
+					}
+					break;
+
+				default:
+					break;
+				}
+				xpp.next();
+			}
+
+			return error;
+		} catch (XmlPullParserException e) {
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
+
+	public static String pingXml() {
+		XmlSerializer sz = Xml.newSerializer();
+		StringWriter writer = new StringWriter();
+		String cardCode = "000013BDBD";
+		try {
+			sz.setOutput(writer);
+			sz.setPrefix("soapenv", EateryConstants.SOAP_PREFIX);
+			sz.setPrefix("mob", EateryConstants.MOB_PREFIX);
+			sz.startTag(EateryConstants.SOAP_PREFIX, "Envelope");
+			sz.startTag(EateryConstants.SOAP_PREFIX, "Header");
+			sz.endTag(EateryConstants.SOAP_PREFIX, "Header");
+			sz.startTag(EateryConstants.SOAP_PREFIX, "Body");
+			sz.startTag(EateryConstants.MOB_PREFIX, "ping");
+			sz.startTag(EateryConstants.MOB_PREFIX, "loginStructure");
+			sz.startTag(EateryConstants.MOB_PREFIX, "cardNumber");
+			sz.text(cardCode);
+			sz.endTag(EateryConstants.MOB_PREFIX, "cardNumber");
+			sz.endTag(EateryConstants.MOB_PREFIX, "loginStructure");
+			sz.endTag(EateryConstants.MOB_PREFIX, "ping");
+			sz.endTag(EateryConstants.SOAP_PREFIX, "Body");
+			sz.endTag(EateryConstants.SOAP_PREFIX, "Envelope");
+			sz.endDocument();
+			return writer.toString();
+		} catch (IOException exception) {
+			Log.e(EateryConstants.SERVICE_LOG_TAG, "[XMLParser] IOException "+ exception.getLocalizedMessage());
+			return null;
+		}
 	}
-		
-	
+
+	public static String parseXMLPing(String xml) {
+		try {
+			Log.i(EateryConstants.SERVICE_LOG_TAG, "[XMLParser] getted ping xml:\n"+ xml);
+			String userName = "";
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			factory.setNamespaceAware(true);
+			XmlPullParser xpp = factory.newPullParser();
+			xpp.setInput(new StringReader(xml));
+			xpp.next();
+			String name = "";
+			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+				switch (xpp.getEventType()) {
+				case XmlPullParser.START_DOCUMENT:
+					break;
+				case XmlPullParser.START_TAG:
+					name = xpp.getName();
+					Log.i(EateryConstants.SERVICE_LOG_TAG, "[XMLParser] getted tag xml:  "+ name);
+					break;
+				case XmlPullParser.END_TAG:
+				case XmlPullParser.TEXT:
+					if (name.equals("enName")) {
+						Log.i(EateryConstants.SERVICE_LOG_TAG, "[XMLParser] getted tag text:  "+xpp.getText());
+						userName = xpp.getText();
+						name="";
+					}
+					break;
+
+				default:
+					break;
+				}
+				xpp.next();
+			}
+			return userName;
+		} catch (XmlPullParserException e) {
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	public static String getMenuXMLRequest() {
 		XmlSerializer sz = Xml.newSerializer();
 		StringWriter writer = new StringWriter();
+		String cardCode = "000013BDBD";
 		try {
 			sz.setOutput(writer);
 			sz.setPrefix("soapenv", EateryConstants.SOAP_PREFIX);
@@ -81,8 +152,11 @@ public class XMLParser {
 			sz.endTag(EateryConstants.SOAP_PREFIX, "Header");
 			sz.startTag(EateryConstants.SOAP_PREFIX, "Body");
 			sz.startTag(EateryConstants.MOB_PREFIX, "getMenu");
-			sz.startTag(EateryConstants.MOB_PREFIX, "getMenuData");
-			sz.endTag(EateryConstants.MOB_PREFIX, "getMenuData");
+			sz.startTag(EateryConstants.MOB_PREFIX, "loginStructure");
+			sz.startTag(EateryConstants.MOB_PREFIX, "cardNumber");
+			sz.text(cardCode);
+			sz.endTag(EateryConstants.MOB_PREFIX, "cardNumber");
+			sz.endTag(EateryConstants.MOB_PREFIX, "loginStructure");
 			sz.endTag(EateryConstants.MOB_PREFIX, "getMenu");
 			sz.endTag(EateryConstants.SOAP_PREFIX, "Body");
 			sz.endTag(EateryConstants.SOAP_PREFIX, "Envelope");
@@ -95,11 +169,11 @@ public class XMLParser {
 
 	public static List<Menu> parseXMLMenu(String xml) {
 		try {
-			List<Menu> menu = new ArrayList<Menu>();			
+			List<Menu> menu = new ArrayList<Menu>();
 			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
 			factory.setNamespaceAware(true);
 			XmlPullParser xpp = factory.newPullParser();
-			xpp.setInput(new StringReader(xml));			
+			xpp.setInput(new StringReader(xml));
 			xpp.next();
 			String name = "";
 			while (!name.equals("menuItems")) {
@@ -113,15 +187,15 @@ public class XMLParser {
 			int dishId = 0;
 			String dishName = "";
 			String dishDescription = "";
-			boolean portioned=false;
+			boolean portioned = false;
 			float price = 0;
 			String rating = "";
-			boolean preorder=false;
+			boolean preorder = false;
 			float availableAmmount = 0;
 			float orderedAmmount = 0;
-			boolean modified=false;
-			int timestamp=0;
-			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT){
+			boolean modified = false;
+			int timestamp = 0;
+			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
 				switch (xpp.getEventType()) {
 				case XmlPullParser.START_DOCUMENT:
 					break;
@@ -130,10 +204,11 @@ public class XMLParser {
 					break;
 				case XmlPullParser.END_TAG:
 					if (xpp.getName().equals("menu")) {
-						Menu m = new Menu(idMenu, date, dishId, dishName, dishDescription,
-								portioned, price, rating, preorder, availableAmmount, orderedAmmount,
-								modified, timestamp);					
-						menu.add(m);						
+						Menu m = new Menu(idMenu, date, dishId, dishName,
+								dishDescription, portioned, price, rating,
+								preorder, availableAmmount, orderedAmmount,
+								modified, timestamp);
+						menu.add(m);
 						idMenu++;
 						Log.i("Dish_log", m.toString());
 
@@ -149,7 +224,7 @@ public class XMLParser {
 						date = dateToUnix(xpp.getText());
 					}
 					if (name.equals("dishId")) {
-						dishId = Integer.parseInt(xpp.getText());
+						dishId = xpp.getText().hashCode(); //Integer.parseInt(xpp.getText());
 					}
 					if (name.equals("name")) {
 						dishName = xpp.getText();
@@ -171,14 +246,14 @@ public class XMLParser {
 						orderedAmmount = (float) Double.parseDouble(xpp
 								.getText());
 					}
-					if (name.equals("preorder")) {
-						//to do protder
+					if (name.equals("preordered")) {
+						portioned=Boolean.parseBoolean(xpp.getText());
 					}
 					if (name.equals("portioned")) {
-						//to do portioned
+						portioned=Boolean.parseBoolean(xpp.getText());
 					}
 					if (name.equals("timestamp")) {
-						//to do timestamp
+						// to do timestamp
 					}
 					break;
 
